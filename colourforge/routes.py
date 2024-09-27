a8c619afbff12b768c4f1a727fcbb78622f5d75d
monkphin <darren.burrows@gmail.com>
2024-09-27 23:30:29 +0100

    added menu bar and started Jinja templating for home.html

--- colourforge/routes.py
+++ colourforge/routes.py
@@ -5,4 +5,4 @@ from colourforge.models import User, Recipes, RecipeStages, RecipeImages, Recipe
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
