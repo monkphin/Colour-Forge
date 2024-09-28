1d5100f2fa9529628cc694177a1af37e2e283e79
monkphin <darren.burrows@gmail.com>
2024-09-28 16:33:51 +0100

    added based structure to home page and created add_recipe page

--- colourforge/routes.py
+++ colourforge/routes.py
@@ -6,3 +6,7 @@ from colourforge.models import User, Recipes, RecipeStages, RecipeImages, Recipe
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
--- colourforge/static/js/script.js
+++ colourforge/static/js/script.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+    // carousel initialization
+    let carousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(carousel);
 });
-        
