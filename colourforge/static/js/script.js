a36c8084a1925347b5949cbbfb8f5aa0fa165d82
monkphin <darren.burrows@gmail.com>
2024-09-29 20:55:50 +0100

    adjusted Javascript to allow for more ccustomised messaging regarding the stage being added

--- colourforge/static/js/script.js
+++ colourforge/static/js/script.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
 
     // Dynamically add new input field after the last input
     $(document).on('click', '.add_field', function() {
-        $('<input type="text" class="input" name="field[]" value="">').insertAfter('.input:last');
+        var stageCount = $('.input').length + 1;  // Get the current count of inputs and increment by 1
+        var newStage = `
+            <div class="input-field col s12">
+                <textarea class="materialize-textarea input" name="instructions[]"></textarea>
+                <label for="instructions">Stage ${stageCount} Instructions</label>
+            </div>
+        `;
+        $('.input:last').parent().after(newStage);  // Insert after the last input
+    });
+    $(document).on('click', '.remove_field', function() {
+        $('.input-field:last').remove();
     });
 });
+
+
+
+
