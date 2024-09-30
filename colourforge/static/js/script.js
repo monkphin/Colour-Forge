f1e20db88bca9b3bab607b44a750e6f6a58af4d1
monkphin <darren.burrows@gmail.com>
2024-09-30 08:11:33 +0100

    added initial image upload config to add_recipe

--- colourforge/static/js/script.js
+++ colourforge/static/js/script.js
@@ -10,8 +10,17 @@ $(document).ready(function(){
         var stageCount = $('.input').length + 1;  // Get the current count of inputs and increment by 1
         var newStage = `
             <div class="input-field col s12">
-                <textarea class="materialize-textarea input" name="instructions[]"></textarea>
-                <label for="instructions">Stage ${stageCount} Instructions</label>
+              <textarea class="materialize-textarea input" name="instructions[]"></textarea>
+              <label for="instructions">Stage ${stageCount} Instructions</label>
+              <div class="file-field input-field">
+                <div class="btn">
+                  <span>File</span>
+                <input type="file" name="images" accept="image/*" multiple>
+                </div>
+                <div class="file-path-wrapper">
+                <input class="file-path validate" type="text" placeholder="Upload one or more images here">
+                </div>
+              </div>
             </div>
         `;
         $('.input:last').parent().after(newStage);  // Insert after the last input
