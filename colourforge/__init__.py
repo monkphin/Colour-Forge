ffac6667d88ec074a086ed2cb4e5bd46b9677025
monkphin <darren.burrows@gmail.com>
2024-10-06 18:23:36 +0100

    amended config to fix for for Heroku deploy

--- colourforge/__init__.py
+++ colourforge/__init__.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 import cloudinary
 import cloudinary.uploader
 from cloudinary.utils import cloudinary_url
-if os.path.exists("env.py"):
-    import env # noqa
 
+if os.path.exists("env.py"):
+    import env  # noqa
 
 app = Flask(__name__)
+
 # Add config - secret keys/DB details etc
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
@@ -16,18 +17,17 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
+    if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # Use the modified URI
 
 # Cloudinary config
 cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"), 
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
     api_secret=os.environ.get("CLOUDINARY_API_SECRET")
 )
 
 db = SQLAlchemy(app)
 
-from colourforge import routes # noqa
+from colourforge import routes  # noqa
