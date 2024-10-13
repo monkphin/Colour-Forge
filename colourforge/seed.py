6a3c41e8073d2b9f70f4ca8f3b8a63123c6475b4
monkphin <darren.burrows@gmail.com>
2024-10-13 23:32:18 +0100

    fixed issues with seed.py

--- colourforge/seed.py
+++ colourforge/seed.py
@@ -5,8 +5,14 @@ def create_default_recipe(user):
     # Create a default recipe
     demo_recipe = Recipe(
         user=user,  # Automatically sets user_id
-        recipe_name='Demo Recipe',
-        recipe_desc='This is a demonstration recipe, to show a rough idea of possible uses'
+        recipe_name="Demo Recipe",
+        recipe_desc=("This is a demonstration recipe, to show a rough idea of "
+                     "possible uses. \r\n"
+                    "\r\n"
+                    "Images can be clicked on to see a larger copy, this will "
+                    "also allow you to open the full sized version of the image "
+                    "in a new window"
+                    )
     )
     db.session.add(demo_recipe)
     db.session.flush()  # Get recipe_id
@@ -19,7 +25,9 @@ def create_default_recipe(user):
             "\r\n"
             "These will honour line breaks via the enter key. \r\n"
             "\r\n"
-            "Images and descriptions are optional, though a placeholder will be added if no image is provided. \r\n"
+            "Images and descriptions are optional, though a placeholder will be "
+            "added if no image is provided. \r\n"
+            "\r\n"
             "Such as with this stage."
         ),
         is_final_stage=False
@@ -27,16 +35,20 @@ def create_default_recipe(user):
     
     stage2 = RecipeStage(
         stage_num=2,
-        instructions='This stage has a user uploaded image with the image description being used as the images alt text.',
+        instructions="This stage has a user uploaded image with the image "
+        "description being used as the images alt text.",
         is_final_stage=False
     )
     
     stage3 = RecipeStage(
         stage_num=3,
         instructions=(
-            "This is the final stage, as such its image will function as the placeholder for the recipe - ideally showing what the end results of the recipe should look like. \r\n"
+            "This is the final stage, as such its image will function as the "
+            "placeholder for the recipe - ideally showing what the end results "
+            "of the recipe should look like. \r\n"
             "\r\n"
-            "Recipes can have a nearly infinite number of stages to allow for some very complex recipes to be created."
+            "Recipes can have a nearly infinite number of stages to allow for "
+            "some very complex recipes to be created."
         ),
         is_final_stage=True
     )
@@ -69,9 +81,10 @@ def create_default_recipe(user):
         public_id='woumsfwwkgycjjqooe3g'
     )
     
-    demo_recipe.stages[0].images.append(image1)
-    demo_recipe.stages[1].images.append(image2)
-    demo_recipe.stages[2].images.append(image3)
+    # Corrected attribute access
+    demo_recipe.stages[0].recipe_images.append(image1)
+    demo_recipe.stages[1].recipe_images.append(image2)
+    demo_recipe.stages[2].recipe_images.append(image3)
 
     # Create or retrieve tags
     tags = ['These', 'Are', 'Tags', "They're", 'Used', 'For', 'Searching']
@@ -88,7 +101,7 @@ def create_default_recipe(user):
     for tag in tag_objects:
         entity_tag = EntityTag(
             recipe=demo_recipe,
-            tag=tag,
+            recipe_tag=tag,
             entity_type='recipe'
         )
         db.session.add(entity_tag)
