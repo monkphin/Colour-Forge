40791c6474d4f59dc3328e9a61e13f0e81f98054
Darren Burrows <monkphin@users.noreply.github.com>
2024-10-19 03:41:34 +0100

    Removed keys from test mail.py

--- testmail.py
+++ testmail.py
@@ -3,13 +3,6 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'noreply@colourforge.co.uk'  # Use your actual Gmail address
-app.config['MAIL_PASSWORD'] = 'xzkk wzok athw ydhp'     # Use your generated App Password
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-mail = Mail(app)
 
 @app.route("/")
 def index():
